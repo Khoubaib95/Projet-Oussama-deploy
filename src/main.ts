bbed970d3f7258ec6602f5d876718a687766cb3d
Khoubaib95 <khoubaibab@gmail.com>
2023-06-02 14:49:09 +0100

    esxport

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   await app.listen(3002 /*  , '192.168.1.15'*/);
