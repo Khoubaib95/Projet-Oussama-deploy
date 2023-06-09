d1bdafd2e6bb752ca8fe23ca38bedc6091d257fb
Khoubaib95 <khoubaibab@gmail.com>
2023-06-09 12:20:20 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.setGlobalPrefix('api/v1');
   await app.listen(3002 /*  , '192.168.1.15'*/);
 }
